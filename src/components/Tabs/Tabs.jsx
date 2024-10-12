25dee2628cee90839fce9f29da54855cc53ef163
Stas <staskapica93@gmail.com>
2024-10-12 19:06:05 +0200

    solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,20 @@
-export const Tabs = () => {};
+export const Tabs = ({ tab, select, set }) => {
+  const check = tab.title === select;
+  let condition = '';
+
+  if (check) {
+    condition = 'go';
+  }
+
+  return (
+    <li data-cy="Tab" className={condition && 'is-active'}>
+      <a
+        href={`#${tab.id}`}
+        data-cy="TabLink"
+        onClick={e => set(e.target.textContent)}
+      >
+        {tab.title}
+      </a>
+    </li>
+  );
+};
